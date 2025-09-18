99e181825b4712ac1356076ef00753e1873e4fca
HackShri <spgrhkadaramandalagi@gmail.com>
2025-09-19 00:44:26 +0530

    eslint config changes

--- Server/eslint.config.js
+++ Server/eslint.config.js
@@ -3,7 +3,7 @@ export default [
     files: ['**/*.js'], //only check .js files in server
     rules: {
       semi: 'error',
-      //'no-unused-vars': 'warn', //if varaibles are enused
+      'no-unused-vars': 'off', //if varaibles are enused
     },
   },
 ];
